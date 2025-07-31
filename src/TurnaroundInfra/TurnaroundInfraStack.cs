ce41cf0b7359889ff7d50cfb284e8e53b3980414
Peter Imade <peterosakpolor661@gmail.com>
2025-07-31 12:26:53 +0100

    Initial commit

---
+++ src/TurnaroundInfra/TurnaroundInfraStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace TurnaroundInfra
+{
+    public class TurnaroundInfraStack : Stack
+    {
+        internal TurnaroundInfraStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
